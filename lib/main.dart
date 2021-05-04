3f740b8bffd66243bdc912f45572a78a0fead599
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-04 22:02:47 +0530

    updates app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:home_solutions_kozhikode/main.gr.dart';
+import 'package:home_solutions_kozhikode/routes/about_us.dart';
 import 'package:home_solutions_kozhikode/routes/landing.dart';
+import 'package:home_solutions_kozhikode/routes/products.dart';
 
 void main() => runApp(HomeSolutions());
 
@@ -9,22 +11,30 @@ void main() => runApp(HomeSolutions());
   replaceInRouteName: 'Page,Route',
   routes: [
     AutoRoute(page: LandingPage, initial: true),
+    AutoRoute(page: ProductsPage),
+    AutoRoute(page: AboutUsPage),
   ],
 )
 class $AppRouter {}
 
 class HomeSolutions extends StatelessWidget {
-  final router = AppRouter();
+  final _router = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Home Solutions',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff243f8f),
+        accentColor: const Color(0xffe50053),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: const Color(0xfffafafa),
+        ),
+        scaffoldBackgroundColor: const Color(0xfffafafa),
       ),
-      routerDelegate: router.delegate(),
-      routeInformationParser: router.defaultRouteParser(),
+      routerDelegate: _router.delegate(),
+      routeInformationParser: _router.defaultRouteParser(),
     );
   }
 }
