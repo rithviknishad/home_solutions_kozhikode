a7c43fbab934512156d80a881186492feaaea358
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-04 22:00:49 +0530

    Co-authored-by: Aswin Murali <aswinmurali.co@gmail.com>

---
+++ lib/routes/about_us.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/main.gr.dart';
+
+class AboutUsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
