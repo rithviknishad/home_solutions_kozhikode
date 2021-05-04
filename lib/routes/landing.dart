d312e7fd92d2be185d77e1b27bb4df7722e535c0
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-04 22:03:08 +0530

    adds landing page

--- lib/routes/landing.dart
+++ lib/routes/landing.dart
@@ -1,17 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/main.gr.dart';
+import 'package:home_solutions_kozhikode/partials/navbar.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Solutions'),
-      ),
       body: Scrollbar(
         child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              MyNavigationBar(),
+            ],
           ),
         ),
       ),
