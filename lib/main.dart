73d5ffbb74ee70b75ae95a338a5387abd5218621
aswinmurali-io <aswinmurali.co@gmail.com>
2021-05-04 11:12:02 +0530

    add auto route, code restruct

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,30 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/main.gr.dart';
+import 'package:home_solutions_kozhikode/route/landing.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(HomeSolutions());
 
-class MyApp extends StatelessWidget {
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: [
+    AutoRoute(page: LandingPage, initial: true),
+  ],
+)
+class $AppRouter {}
+
+class HomeSolutions extends StatelessWidget {
+  final _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'Home Solutions',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
