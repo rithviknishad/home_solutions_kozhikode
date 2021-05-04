31c5715209486b6dfb7b7bf7b08579b8a935562c
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-04 22:02:57 +0530

    updates app router generated code

--- lib/main.gr.dart
+++ lib/main.gr.dart
@@ -7,7 +7,9 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
+import 'routes/about_us.dart' as _i5;
 import 'routes/landing.dart' as _i3;
+import 'routes/products.dart' as _i4;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
@@ -18,16 +20,39 @@ class AppRouter extends _i1.RootStackRouter {
     LandingRoute.name: (routeData) {
       return _i1.MaterialPageX<dynamic>(
           routeData: routeData, child: _i3.LandingPage());
+    },
+    ProductsRoute.name: (routeData) {
+      return _i1.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i4.ProductsPage());
+    },
+    AboutUsRoute.name: (routeData) {
+      return _i1.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i5.AboutUsPage());
     }
   };
 
   @override
-  List<_i1.RouteConfig> get routes =>
-      [_i1.RouteConfig(LandingRoute.name, path: '/')];
+  List<_i1.RouteConfig> get routes => [
+        _i1.RouteConfig(LandingRoute.name, path: '/'),
+        _i1.RouteConfig(ProductsRoute.name, path: '/products-page'),
+        _i1.RouteConfig(AboutUsRoute.name, path: '/about-us-page')
+      ];
 }
 
 class LandingRoute extends _i1.PageRouteInfo {
   const LandingRoute() : super(name, path: '/');
 
   static const String name = 'LandingRoute';
+}
+
+class ProductsRoute extends _i1.PageRouteInfo {
+  const ProductsRoute() : super(name, path: '/products-page');
+
+  static const String name = 'ProductsRoute';
+}
+
+class AboutUsRoute extends _i1.PageRouteInfo {
+  const AboutUsRoute() : super(name, path: '/about-us-page');
+
+  static const String name = 'AboutUsRoute';
 }
