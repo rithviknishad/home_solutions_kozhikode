afcbdd113c77eaf1fae306f25051aac4c377197c
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-05 20:14:20 +0530

    adds product page

--- lib/routes/products.dart
+++ lib/routes/products.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:home_solutions_kozhikode/partials/navigation_bar.dart';
+import 'package:home_solutions_kozhikode/partials/wrapper.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              MyNavigationBar(),
-              SizedBox.fromSize(
-                size: size,
-                child: Center(
-                  child: Text("Hello"),
-                ),
-              ),
-            ],
-          ),
+    return PageWrapper(
+      page: SizedBox.fromSize(
+        size: size,
+        child: Center(
+          child: Text("Hello"),
         ),
       ),
     );
