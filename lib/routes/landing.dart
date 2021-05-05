fd2e46445012750899a003ff902e9fb5836c37b0
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-05 15:19:49 +0530

    updates landing

--- lib/routes/landing.dart
+++ lib/routes/landing.dart
@@ -5,14 +5,21 @@ import 'package:home_solutions_kozhikode/partials/navigation_bar.dart';
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Scrollbar(
         child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               MyNavigationBar(),
+              SizedBox.fromSize(
+                size: size,
+                child: Center(
+                  child: Text("Hello"),
+                ),
+              ),
             ],
           ),
         ),
