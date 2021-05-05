13d4a4b15772b22eaadf5d3b87faf7afe38aef84
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-05 15:19:54 +0530

    updates product

--- lib/routes/products.dart
+++ lib/routes/products.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/partials/navigation_bar.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              MyNavigationBar(),
+              SizedBox.fromSize(
+                size: size,
+                child: Center(
+                  child: Text("Hello"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
