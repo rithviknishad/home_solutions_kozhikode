ac583c6e76d842a8b5ae158a16fc4a3b50f790a7
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-05 20:13:44 +0530

    auto route maintenance

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:home_solutions_kozhikode/main.gr.dart';
-import 'package:home_solutions_kozhikode/routes/about_us.dart';
+import 'package:home_solutions_kozhikode/routes/about.dart';
 import 'package:home_solutions_kozhikode/routes/contact.dart';
 import 'package:home_solutions_kozhikode/routes/landing.dart';
 import 'package:home_solutions_kozhikode/routes/products.dart';
