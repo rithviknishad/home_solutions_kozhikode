9b276ad8e42a484c6bf9310cbab0fb5cb5d57405
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-05 20:14:11 +0530

    adds contact page

---
+++ lib/routes/contact.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/partials/wrapper.dart';
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageWrapper(
+      page: Container(
+        child: Text("Call me"),
+      ),
+    );
+  }
+}
