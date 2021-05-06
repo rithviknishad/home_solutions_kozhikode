6a820999296a4bd780d89ec34f61badbdb7aba4b
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-06 12:12:02 +0530

    updates navigation bar to use ham burger

--- lib/partials/navigation_bar.dart
+++ lib/partials/navigation_bar.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:home_solutions_kozhikode/main.gr.dart';
+import 'package:home_solutions_kozhikode/home_solutions.dart';
+import 'package:home_solutions_kozhikode/partials/my_page.dart';
+import 'package:home_solutions_kozhikode/router.gr.dart';
 
 class MyNavigationBar extends StatelessWidget {
   const MyNavigationBar._();
@@ -9,10 +11,14 @@ class MyNavigationBar extends StatelessWidget {
 
   static const instance = const MyNavigationBar._();
 
+  static const _width = 1100;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final theme = Theme.of(context);
+
+    print(size);
 
     return Container(
       decoration: BoxDecoration(
@@ -29,7 +35,10 @@ class MyNavigationBar extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Spacer(flex: 3),
+          if (size.width > _width)
+            SizedBox(width: (size.width - _width) / 2)
+          else
+            SizedBox(width: 20),
 
           // Home Solutions LOGO
           Padding(
@@ -43,10 +52,10 @@ class MyNavigationBar extends StatelessWidget {
           ),
 
           // Home Solutions Title
-          if (size.width > 800)
+          if (size.width > 1000)
             Text(
               "Home Solutions",
-              style: theme.textTheme.headline5?.copyWith(
+              style: theme.textTheme.headline4?.copyWith(
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                   shadows: [Shadow(color: Colors.black26, blurRadius: 12)]),
@@ -54,55 +63,57 @@ class MyNavigationBar extends StatelessWidget {
 
           Spacer(flex: 9),
           // Navigation Buttons
-          for (final b in navigationPageButtons) ...[b, const Spacer(flex: 1)],
-          Spacer(flex: 3),
+          if (size.width < 745)
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                color: Colors.white,
+                iconSize: 32,
+                onPressed: Scaffold.of(context).openEndDrawer,
+              ),
+            )
+          else
+            for (final page in HomeSolutions.pages) _NavbarPageButton(page),
+          if (size.width > _width) SizedBox(width: (size.width - _width) / 2),
         ],
       ),
     );
   }
 
-  static final navigationPageButtons = [
-    _PageButton("Home", () => Home()),
-    _PageButton("Products", () => Products()),
-    _PageButton("Services", () => Services()),
-    _PageButton("About", () => About()),
-    _PageButton("Contact", () => Contact()),
-  ];
-
   Image get logo =>
-      Image.asset('assets/logo_borderless.png', height: 86, isAntiAlias: true);
+      Image.asset('assets/logo_borderless.png', height: 128, isAntiAlias: true);
 }
 
-class _PageButton extends StatelessWidget {
-  final String name;
-  final PageRouteInfo Function() route;
+class _NavbarPageButton extends StatelessWidget {
+  final MyPage page;
 
-  const _PageButton(this.name, this.route);
+  const _NavbarPageButton(this.page);
 
   @override
   Widget build(BuildContext context) {
-    final isCurrent = context.routeData.name.replaceAll('_', ' ') == name;
+    final isCurrent = context.routeData.name.replaceAll('_', ' ') == page.name;
 
-    return Container(
-      padding: const EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: TextButton(
         // style: ButtonStyle(overlayColor: MaterialStateProperty.all()),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
-            name,
+            page.name,
             style: TextStyle(
               fontWeight: isCurrent ? FontWeight.bold : FontWeight.normal,
             ),
           ),
         ),
-        onPressed: () => pushPage(using: context.router),
+        onPressed: () => _open(using: context.router),
       ),
     );
   }
 
-  void pushPage({required StackRouter using}) {
-    final route = this.route();
+  void _open({required StackRouter using}) {
+    final route = this.page.opens();
 
     if (route.routeName != using.current.name) {
       using.push(route);
