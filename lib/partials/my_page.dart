0d143bfff051af5ee73db2b6d7b664ef9519c42d
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-06 17:36:29 +0530

    adds currentPage detection to mypage

--- lib/partials/my_page.dart
+++ lib/partials/my_page.dart
@@ -1,13 +1,22 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
 class MyPage {
   final String name;
+  final Icon icon;
   final PageRouteInfo Function() opens;
 
-  const MyPage(this.name, {required this.opens});
+  const MyPage({
+    required this.name,
+    required this.icon,
+    required this.opens,
+  });
 
   void open({required StackRouter using}) {
     final route = opens();
     if (route.routeName != using.current.name) using.push(route);
   }
+
+  bool isCurrent(BuildContext context) =>
+      context.routeData.name.replaceAll('_', ' ') == name;
 }
