f8fa2a28571b1f3593664bc75b2bab1c8c4b6a7e
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-07 17:48:09 +0530

    remove routes

--- lib/router.dart
+++ lib/router.dart
@@ -9,10 +9,6 @@ import 'package:home_solutions_kozhikode/routes/services.dart';
   replaceInRouteName: 'Page,Route',
   routes: [
     AutoRoute(page: LandingPage, initial: true, name: "Home"),
-    AutoRoute(page: ProductsPage, path: '/products', name: "Products"),
-    AutoRoute(page: ServicesPage, path: '/services', name: "Services"),
-    AutoRoute(page: AboutPage, path: '/about', name: "About"),
-    AutoRoute(page: ContactPage, path: '/contact', name: "Contact"),
   ],
 )
 class $AppRouter {}
