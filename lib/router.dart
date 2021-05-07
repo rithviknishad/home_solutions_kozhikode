43000257835bc6b8aef0c1a7df27ebc5d47da936
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-07 17:48:23 +0530

    remove imports

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:home_solutions_kozhikode/routes/about.dart';
-import 'package:home_solutions_kozhikode/routes/contact.dart';
 import 'package:home_solutions_kozhikode/routes/landing.dart';
-import 'package:home_solutions_kozhikode/routes/products.dart';
-import 'package:home_solutions_kozhikode/routes/services.dart';
 
 @CustomAutoRouter(
   replaceInRouteName: 'Page,Route',
