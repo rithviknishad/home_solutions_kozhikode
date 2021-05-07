37df5f29ebd68cb03dd2f1bd295166ab7971cfd7
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-07 10:00:25 +0530

    adds sliver app bar delegate

--- lib/routes/landing.dart
+++ lib/routes/landing.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:home_solutions_kozhikode/partials/wrapper.dart';
@@ -9,11 +11,77 @@ class LandingPage extends StatelessWidget {
 
     return PageWrapper(
       appBar: null,
-      child: SizedBox.fromSize(
-        size: size,
-        child: Center(
-          child: Text("Hello"),
-        ),
+      child: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+              delegate: _SliverAppBarDelegate(), pinned: true),
+        ],
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  double get maxExtent => 300.0;
+
+  @override
+  double get minExtent => 56.0;
+
+  @override
+  bool shouldRebuild(oldDelegate) => true;
+
+  double scrollAnimationValue(double shrinkOffset) {
+    double maxScrollAllowed = maxExtent - minExtent;
+
+    return ((maxScrollAllowed - shrinkOffset) / maxScrollAllowed)
+        .clamp(0, 1)
+        .toDouble();
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final double visibleMainHeight = max(maxExtent - shrinkOffset, minExtent);
+    final double animationVal = scrollAnimationValue(shrinkOffset);
+
+    return Container(
+      height: visibleMainHeight,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(color: Colors.blue),
+          Opacity(
+              opacity: animationVal,
+              child: Image.network(
+                "https://picsum.photos/500/500",
+                fit: BoxFit.cover,
+              )),
+          Positioned(
+            bottom: 0.0,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    flex: (animationVal * 100).toInt(),
+                    child: Container(),
+                  ),
+                  Text("Lorem Ipsum Dolar Sit"),
+                  Expanded(
+                    flex: 100,
+                    child: Container(),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
