f2170dce5fca42e734ba60169bb310d96c5d1412
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-08 12:07:06 +0530

    adds constants

---
+++ lib/constants/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = const Color(0xff243f8f);
+const accentColor = const Color(0xff12143c);
+const subtleWhite = Color(0xfffafafa);
+const fontFamily = 'Poppins';
+const textStyle = TextStyle(fontFamily: fontFamily);
