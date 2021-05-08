c4306db274ad4c30661bab5da78b19b4998f79da
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-08 19:51:56 +0530

    adds welcome section

---
+++ lib/partials/sections/welcome.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:home_solutions_kozhikode/home_solutions.dart';
+import 'package:home_solutions_kozhikode/partials/sections/section.dart';
+
+class WelcomeSection extends MySection {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.grey[300]!,
+            Colors.grey[200]!,
+          ],
+        ),
+      ),
+      height: size.height * 0.9,
+      child: Center(
+        child: Text(HomeSolutions.catchPrase, style: theme.textTheme.headline3),
+      ),
+    );
+  }
+}
