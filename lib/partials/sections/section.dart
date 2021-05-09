f6cdde436593b88115a97132ace09286fa0b2c71
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-09 12:03:12 +0530

    adds section.dart

--- lib/partials/sections/section.dart
+++ lib/partials/sections/section.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-abstract class MySection extends StatelessWidget {}
+abstract class MySection extends StatelessWidget {
+  int get maxDisplayWidth => 1140;
+}
