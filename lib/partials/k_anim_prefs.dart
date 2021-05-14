c23d3a3c221092b4dbbdde4e2a72f57e3f224c14
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-14 18:23:19 +0530

    adds anim pref

---
+++ lib/partials/k_anim_prefs.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_animator/flutter_animator.dart';
+
+const slowAnimation = AnimationPreferences(
+  duration: Duration(milliseconds: 800),
+);
+
+const normalAnimation = AnimationPreferences(
+  duration: Duration(milliseconds: 500),
+);
+
+const swiftAnimation = AnimationPreferences(
+  duration: Duration(milliseconds: 300),
+);
