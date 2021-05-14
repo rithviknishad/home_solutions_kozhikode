3be277c73e0614bc555eb4c8b865f36702ca2023
Rithvik Nishad <rithvikn2001@gmail.com>
2021-05-14 18:23:25 +0530

    adds about section

---
+++ lib/partials/sections/about.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_animator/flutter_animator.dart';
+import 'package:home_solutions_kozhikode/partials/k_anim_prefs.dart';
+import 'package:home_solutions_kozhikode/partials/sections/section.dart';
+
+class AboutUsSection extends MySection {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      color: theme.primaryColor,
+      padding: EdgeInsets.symmetric(
+        horizontal: max((size.width - maxDisplayWidth) / 2, 0) + 10,
+      ).copyWith(top: 30, bottom: 60),
+      width: size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
+            child: FadeInUp(
+              preferences: normalAnimation,
+              child: Text(
+                "We are",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FadeInUp(
+              preferences: slowAnimation,
+              child: Text(
+                "${r"    // ** \\   NO CONTENT   // ** \\    " * 4}\n // TODO: set_onContentAvailable: TextStyle(color: Colors.white70)",
+                style: TextStyle(color: Colors.yellow),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
