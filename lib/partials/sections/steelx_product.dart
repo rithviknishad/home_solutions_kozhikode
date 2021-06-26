3688da0f9175b43f94dfd73fe5ff45b35b5a82e8
rithviknishad <rithvikn2001@gmail.com>
2021-06-26 09:48:22 +0530

    scalable features

--- lib/partials/sections/steelx_product.dart
+++ lib/partials/sections/steelx_product.dart
@@ -6,6 +6,40 @@ import 'package:home_solutions_kozhikode/partials/k_anim_prefs.dart';
 import 'package:home_solutions_kozhikode/partials/sections/section.dart';
 
 class SteelxProductSection extends MySection {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: max((size.width - maxDisplayWidth) / 2, 0) + 10,
+      ).copyWith(top: 30, bottom: 60),
+      width: size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _Heading(),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Image(
+              image: AssetImage('assets/steelx_banner.png'),
+              isAntiAlias: true,
+              filterQuality: FilterQuality.high,
+              height: 600,
+            ),
+          ),
+          SizedBox(height: 30.0),
+          _SteelxProductFeatures(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SteelxProductFeatures extends StatelessWidget {
+  const _SteelxProductFeatures({Key? key}) : super(key: key);
+
   static const _features = {
     "No Bacteria, Fungus & Algae": "no_bacteria_fungae_and_algae.png",
     "Safe Healthy & Hygienic": "safe_healthy_and_hygienic.png",
@@ -21,48 +55,19 @@ class SteelxProductSection extends MySection {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final size = MediaQuery.of(context).size;
-
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.symmetric(
-        horizontal: max((size.width - maxDisplayWidth) / 2, 0) + 10,
-      ).copyWith(top: 30, bottom: 60),
-      width: size.width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _Heading(),
-
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Image(
-              image: AssetImage('assets/steelx_banner.png'),
-              isAntiAlias: true,
-              filterQuality: FilterQuality.high,
-              height: 600,
-            ),
+    return Wrap(
+      alignment: WrapAlignment.center,
+      runAlignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      spacing: 30,
+      runSpacing: 30,
+      children: [
+        for (final f in _features.entries)
+          _FeatureTile(
+            description: f.key,
+            asset: 'assets/steelx_features/${f.value}',
           ),
-          SizedBox(height: 30.0),
-
-          // Features
-          Wrap(
-            alignment: WrapAlignment.center,
-            runAlignment: WrapAlignment.center,
-            crossAxisAlignment: WrapCrossAlignment.center,
-            spacing: 30,
-            runSpacing: 30,
-            children: [
-              for (final f in _features.entries)
-                _FeatureTile(
-                  description: f.key,
-                  asset: 'assets/steelx_features/${f.value}',
-                ),
-            ],
-          )
-        ],
-      ),
+      ],
     );
   }
 }
@@ -107,7 +112,7 @@ class _FeatureTile extends StatelessWidget {
 }
 
 class _Heading extends StatelessWidget {
-  const _Heading({Key? key}) : super(key: key);
+  const _Heading();
 
   @override
   Widget build(BuildContext context) {
