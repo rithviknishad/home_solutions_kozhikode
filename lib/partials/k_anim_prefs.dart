60332f77944be5761d8ba76763bb029bd625ab95
rithviknishad <rithvikn2001@gmail.com>
2021-07-06 15:07:56 +0530

    upadate anim prefs

--- lib/partials/k_anim_prefs.dart
+++ lib/partials/k_anim_prefs.dart
@@ -1,13 +1,18 @@
 import 'package:flutter_animator/flutter_animator.dart';
 
+const _defaultOffset = Duration(milliseconds: 500);
+
 const slowAnimation = AnimationPreferences(
   duration: Duration(milliseconds: 800),
+  offset: _defaultOffset,
 );
 
 const normalAnimation = AnimationPreferences(
   duration: Duration(milliseconds: 500),
+  offset: _defaultOffset,
 );
 
 const swiftAnimation = AnimationPreferences(
   duration: Duration(milliseconds: 300),
+  offset: _defaultOffset,
 );
